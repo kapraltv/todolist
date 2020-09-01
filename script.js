5faaef42fbff23a5fdf5408baa7c38a6e43bc248
kapraltv <69194044+kapraltv@users.noreply.github.com>
2020-09-01 12:29:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,236 @@
+const tasks = [
+  {
+    _id: "5d2ca9e2e03d40b326596aa7",
+    completed: true,
+    body:
+      "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
+    title: "Eu ea incididunt sunt consectetur fugiat non.",
+  },
+  {
+    _id: "5d2ca9e29c8a94095c1288e0",
+    completed: false,
+    body:
+      "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
+    title:
+      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
+  },
+  {
+    _id: "5d2ca9e2e03d40b3232496aa7",
+    completed: true,
+    body:
+      "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
+    title: "Eu ea incididunt sunt consectetur fugiat non.",
+  },
+  {
+    _id: "5d2ca9e29c8a94095564788e0",
+    completed: false,
+    body:
+      "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
+    title:
+      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.",
+  },
+];
+
+(function (arrOfTasks) {
+  const objOfTasks = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  const theme = {
+    default: {
+      "--base-text-color": "black",
+      "--header-bg": "blue",
+      "--header-text-color": "white",
+      "--default-btn-bg": "blue",
+      "--default-btn-text-color": "white",
+      "--default-btn-hover-bg": "blue",
+      "--default-btn-border-color": "blue",
+      "--danger-btn-bg": "red",
+      "--danger-btn-text-color": "white",
+      "--danger-btn-hover-bg": "red",
+      "--input-border-color": "#ced4da",
+      "--input-bg-color": "yellow",
+      "--input-text-color": "black",
+      "--input-focus-bg-color": "white",
+      "--input-focus-text-color": "#495057",
+      "--input-focus-border-color": "#80bdff",
+      "--input-focus-box-shadow": "0 0 0 0.2rem rgba(0, 123, 255, 0.25)",
+    },
+    dark: {
+      "--base-text-color": "black",
+      "--header-bg": "black",
+      "--header-text-color": "white",
+      "--default-btn-bg": "black",
+      "--default-btn-text-color": "white",
+      "--default-btn-hover-bg": "blue",
+      "--default-btn-border-color": "blue",
+      "--danger-btn-bg": "red",
+      "--danger-btn-text-color": "white",
+      "--danger-btn-hover-bg": "red",
+      "--input-border-color": "#ced4da",
+      "--input-bg-color": "yellow",
+      "--input-text-color": "black",
+      "--input-focus-bg-color": "white",
+      "--input-focus-text-color": "#495057",
+      "--input-focus-border-color": "#80bdff",
+      "--input-focus-box-shadow": "0 0 0 0.2rem rgba(0, 123, 255, 0.25)",
+    },
+    light: {
+      "--base-text-color": "black",
+      "--header-bg": "pink",
+      "--header-text-color": "white",
+      "--default-btn-bg": "pink",
+      "--default-btn-text-color": "white",
+      "--default-btn-hover-bg": "blue",
+      "--default-btn-border-color": "blue",
+      "--danger-btn-bg": "red",
+      "--danger-btn-text-color": "white",
+      "--danger-btn-hover-bg": "red",
+      "--input-border-color": "#ced4da",
+      "--input-bg-color": "yellow",
+      "--input-text-color": "black",
+      "--input-focus-bg-color": "white",
+      "--input-focus-text-color": "#495057",
+      "--input-focus-border-color": "#80bdff",
+      "--input-focus-box-shadow": "0 0 0 0.2rem rgba(0, 123, 255, 0.25)",
+    },
+  };
+
+  let lastSelectedTheme = localStorage.getItem("app.theme") || "default";
+
+  // Elements UI
+  const listContainer = document.querySelector(
+    ".tasks-list-section .list-group"
+  );
+
+  const form = document.forms["AddTask"];
+  const inputTitle = form.elements["title"];
+  const inputBody = form.elements["body"];
+  const themeSelect = document.getElementById("themeSelect");
+
+  // Events
+  setTheme(lastSelectedTheme);
+  renderAllTasks(objOfTasks);
+  form.addEventListener("submit", onFormSubmitHandler);
+  listContainer.addEventListener("click", onDeleteHandler);
+  themeSelect.addEventListener("change", onThemeSelectHandler);
+
+  function renderAllTasks(tasksList) {
+    if (!tasksList) {
+      console.error("Отсутствует список задач");
+      return;
+    }
+
+    const fragment = document.createDocumentFragment();
+    Object.values(tasksList).forEach((task) => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+    listContainer.appendChild(fragment);
+  }
+
+  function listItemTemplate({ _id, title, body } = {}) {
+    const li = document.createElement("li");
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "align-items-center",
+      "flex-wrap",
+      "mt-2"
+    );
+
+    li.setAttribute("data-task-id", _id);
+
+    const span = document.createElement("span");
+    span.textContent = title;
+    span.style.fontWeight = "bold";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete content";
+    deleteBtn.classList.add("btn", "btn-danger", "ml-auto", "delete-btn");
+
+    const article = document.createElement("p");
+    article.textContent = body;
+    article.classList.add("mt-2", "w-100");
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    return li;
+  }
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+    const titleValue = inputTitle.value;
+    const bodyValue = inputBody.value;
+
+    if (!titleValue || !bodyValue) {
+      alert("Write title and body");
+      return;
+    }
+
+    const task = createNewTask(titleValue, bodyValue);
+    const listItem = listItemTemplate(task);
+    listContainer.insertAdjacentElement("afterbegin", listItem);
+    form.reset();
+  }
+
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`,
+    };
+
+    objOfTasks[newTask._id] = newTask;
+    return {
+      ...newTask,
+    };
+  }
+
+  function deleteTask(id) {
+    const { title } = objOfTasks[id];
+    const isConfirm = confirm(`Точно вы хотите удалить задачу: ${title}`);
+    if (!isConfirm) return isConfirm;
+    delete objOfTasks[id];
+    return isConfirm;
+  }
+
+  function deleteTaskFromHtml(confirmed, el) {
+    if (!confirmed) return;
+    el.remove();
+  }
+
+  function onDeleteHandler({ target }) {
+    if (target.classList.contains("delete-btn")) {
+      const parent = target.closest("[data-task-id]");
+      const id = parent.dataset.taskId;
+      const confirmed = deleteTask(id);
+      deleteTaskFromHtml(confirmed, parent);
+    }
+  }
+
+  function onThemeSelectHandler(e) {
+    const selectedTheme = themeSelect.value;
+    const isConfirmed = confirm(
+      `Вы действительно хотите изменить тему: ${selectedTheme}`
+    );
+    if (!isConfirmed) {
+      themeSelect.value = lastSelectedTheme;
+      return;
+    }
+    setTheme(selectedTheme);
+    lastSelectedTheme = selectedTheme;
+    localStorage.setItem("app_theme", selectedTheme);
+  }
+
+  function setTheme(name) {
+    const selectedThemeObj = theme[name];
+    Object.entries(selectedThemeObj).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    });
+  }
+})(tasks);
